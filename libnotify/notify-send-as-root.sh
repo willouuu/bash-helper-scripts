217a77d6ef50ae2e567bc1fc8ce1fad96fcd2efc
William VINCENT <william66750@gmail.com>
2017-04-19 13:39:26 +0200

    Update notify-send-as-root.sh
    send notify all users

--- libnotify/notify-send-as-root.sh
+++ libnotify/notify-send-as-root.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-#
-# This script shows how to send a libnotify message
-# to a specific user.
-#
-# It looks for a process that was started by the user and is connected to dbus.
 
-# process to determine DBUS_SESSION_BUS_ADDRESS
-USER_DBUS_PROCESS_NAME="gconfd-2"
+# get list of current users
+for userLast in $(who | sort -u -k1,1 | awk '{print $1}'); do
 
-USER="makubi"
-NOTIFY_SEND_BIN="/usr/bin/notify-send"
+        USER="$userLast"
+        NOTIFY_SEND_BIN="/usr/bin/notify-send"
 
-TITLE="title"
-MESSAGE="notify message"
+        TITLE="Mise à jour en cours"
 
-# get pid of user dbus process
-DBUS_PID=`ps ax | grep $USER_DBUS_PROCESS_NAME | grep -v grep | awk '{ print $1 }'`
+        MESSAGE="Merci de ne pas eteindre l'ordinateur"
 
-# get DBUS_SESSION_BUS_ADDRESS variable
-DBUS_SESSION=`grep -z DBUS_SESSION_BUS_ADDRESS /proc/$DBUS_PID/environ | sed -e s/DBUS_SESSION_BUS_ADDRESS=//`
+        # get pid of user dbus process
+        DBUS_PID=`ps ax | grep "gconfd-2" | grep -v grep | awk '{ print $1 }'`
 
-# send notify
-DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION su -c "$NOTIFY_SEND_BIN \"$TITLE\" \"$MESSAGE\"" $USER
+        # get DBUS_SESSION_BUS_ADDRESS variable
+        DBUS_SESSION=`grep -z DBUS_SESSION_BUS_ADDRESS /proc/$DBUS_PID/environ | sed -e s/DBUS_SESSION_BUS_ADDRESS=//`
 
+        # send notify
+        DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION su -c "$NOTIFY_SEND_BIN \"$TITLE\" \"$MESSAGE\"" $USER
+
+done
